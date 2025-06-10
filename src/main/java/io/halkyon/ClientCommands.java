285ee55674e277148d715b2ebcfacf530a94ad62
cmoulliard <cmoulliard@redhat.com>
2025-06-10 16:30:06 +0200

    Recreate the ClientConfig
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- src/main/java/io/halkyon/ClientCommands.java
+++ src/main/java/io/halkyon/ClientCommands.java
@@ -1,6 +1,8 @@
 package io.halkyon;
 
 import io.quarkus.runtime.QuarkusApplication;
+import io.smallrye.config.SmallRyeConfig;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.ConfigProvider;
 import picocli.CommandLine;
@@ -8,8 +10,10 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.concurrent.Callable;
 
+@ApplicationScoped
 @Command(name = "mycli-app", mixinStandardHelpOptions = true, version = "mycli-app 1.0")
 public class ClientCommands implements Callable, QuarkusApplication {
 
@@ -27,18 +31,6 @@ public class ClientCommands implements Callable, QuarkusApplication {
 
     @Override
     public Integer call() throws Exception {
-        /*
-        if (externalConfigFile != null) {
-            if (!externalConfigFile.exists() || !externalConfigFile.isFile()) {
-                System.err.println("Error: External config file not found or is not a regular file: " + externalConfigFile.getAbsolutePath());
-                return 1;
-            }
-            System.setProperty("quarkus.config.locations", "file:" + externalConfigFile.getAbsolutePath());
-            System.out.println("Loading external config from: " + externalConfigFile.getAbsolutePath());
-        } else {
-            System.out.println("No external config file specified. Using only internal application.yaml.");
-        }
-        */
         var props = ConfigProvider.getConfig().getPropertyNames();
         props.forEach(n -> {
             if (n.startsWith("kind")) {
@@ -47,6 +39,12 @@ public class ClientCommands implements Callable, QuarkusApplication {
                 System.out.println("\n");
             }
         });
+
+        System.out.println("## Show ClientConfig");
+        ClientConfig cfg = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class).getConfigMapping(ClientConfig.class);
+        System.out.println("Kubeversion: " + cfg.kubernetesVersion());
+        System.out.println("Name: " + cfg.name());
+
         return 0;
     }
 }
