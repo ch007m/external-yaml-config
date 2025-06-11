6bf29453207205c67c4ce34cbe81fe83143c8d4e
cmoulliard <cmoulliard@redhat.com>
2025-06-11 17:19:54 +0200

    Customize the SR Config

--- src/main/java/io/halkyon/ClientCommands.java
+++ src/main/java/io/halkyon/ClientCommands.java
@@ -2,10 +2,14 @@ package io.halkyon;
 
 import io.smallrye.config.SmallRyeConfig;
 import io.smallrye.config.SmallRyeConfigBuilder;
+import org.eclipse.microprofile.config.ConfigProvider;
+import org.eclipse.microprofile.config.spi.ConfigSource;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 import static io.smallrye.config.SmallRyeConfig.SMALLRYE_CONFIG_LOCATIONS;
@@ -23,14 +27,30 @@ public class ClientCommands implements Callable {
     public Integer call() throws Exception {
         if (externalConfigFile != null) {
             System.out.println("Yaml config file: " + externalConfigFile.getAbsolutePath());
-            SmallRyeConfig config = new SmallRyeConfigBuilder()
-                .addDefaultSources()
-                .addDiscoveredSources()
-                .addDefaultInterceptors()
-                .withDefaultValue(SMALLRYE_CONFIG_LOCATIONS, externalConfigFile.getAbsolutePath())
+            SmallRyeConfig config = ConfigProvider.getConfig().unwrap(SmallRyeConfig.class);
+            SmallRyeConfig ClientConfig = new SmallRyeConfigBuilder()
                 .withMapping(ClientConfig.class)
+                .withDefaultValue(SMALLRYE_CONFIG_LOCATIONS, externalConfigFile.getAbsolutePath())
+                .withSources(new ConfigSource() {
+                    @Override
+                    public Set<String> getPropertyNames() {
+                        Set<String> properties = new HashSet<>();
+                        config.getPropertyNames().forEach(properties::add);
+                        return properties;
+                    }
+
+                    @Override
+                    public String getValue(final String propertyName) {
+                        return config.getRawValue(propertyName);
+                    }
+
+                    @Override
+                    public String getName() {
+                        return "Client Config";
+                    }
+                })
                 .build();
-            cfg = config.getConfigMapping(ClientConfig.class);
+            cfg = ClientConfig.getConfigMapping(ClientConfig.class);
             System.out.println("Client Config content: ");
             System.out.println("Kube version: " + cfg.kubernetesVersion().get());
             System.out.println("Name: " + cfg.name());
